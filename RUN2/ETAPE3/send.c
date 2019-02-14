a569cb5b039755df08f9074a66091f3319c46379
psyqozz <dylan.juzaine@gmail.com>
2019-02-14 06:35:18 -0500

    step 3 clear

--- RUN2/ETAPE3/send.c
+++ RUN2/ETAPE3/send.c
@@ -1,8 +1,8 @@
 /*
-** ETNA PROJECT, 10/02/2019 groupe de prylut_s
+** ETNA PROJECT, 31/01/2019 groupe de prylut_s
 ** BombermanRun2
 ** File description:
-**      etape 3 , multiclient
+**      etape 2 , 
 */
 
 #include <string.h>
@@ -20,32 +20,30 @@
  *
  * \return int
  */
-int main(int argc, char **argv)
+
+int main(int argc, char* argv[])
 {
   char *ip = NULL;
   int port = 0;
   int mysocket;
-  int client;
-  socklen_t client_addr_len;
   struct sockaddr_in server;
-  char message[128];
-
+  char message[128] ;
+  char server_reply[128];
+  int firstMessage = 0;
+  
   if (argc != 3) {
-		printf("usage : %s IP PORT\n", argv[0]);
-		return -1;
-	}
-
+      printf("usage : %s IP PORT\n", argv[0]);
+      return -1;
+  }
   ip = argv[1];
   port = atoi(argv[2]);
-
-	// printf("IP : %s\tPORT : %d\n", ip, port);
-
-
+  
   mysocket = socket(AF_INET, SOCK_STREAM, 0);
   if (mysocket < 0) {
       perror("socket()");
       return -1;
   }
+  
 
   server.sin_addr.s_addr = inet_addr(ip);
   server.sin_port = htons(port);
@@ -55,18 +53,32 @@ int main(int argc, char **argv)
       perror("connect()");
       return 1;
   }
+  printf("[+] connected to server  \n");
 
   while (1) {
-      memset(message, '\0', 128);
-      fgets(message, 128, stdin);
-      
-      if (send(mysocket, message, strlen(message), 0) < 0) {
-          puts("send failed");
-          close(mysocket);
-          return 1;
-        }
+    memset(message, '\0', 128);
+    fgets(message, 128, stdin);
+    if (send(mysocket, message, strlen(message), 0) < 0) {
+        puts("[-] send failed\n");
+        close(mysocket);
+        return 1;
+    }
+    printf("sended %s\n", message);
     
-        printf("sended %s\n", message);
+    if (strcmp(message, "exit\n") == 0) {
+        close(mysocket);
+        printf("[-] disconnected\n");
+        exit(1);
+    }
+    if (recv(mysocket, server_reply, 2000, 0) <= 0 )  {
+        puts("server down...\n");
+        break;
+    }
+    printf("server reply : %s \n", server_reply);
+    if (strcmp(server_reply, "[!] full server") == 0) {
+        printf("[-] disconnected\n");
+        break;
+    }
   }
 
   close(mysocket);
